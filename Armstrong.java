f3c328c912bee78d1e205fcb13f9a76132450205
jayasuriyacj <jayasuriyac070@gmail.com>
2018-09-03 13:45:15 +0530

    Create Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,24 @@
+class Armstrong
+{
+public static void main(String args[])
+{
+int a=0;
+int b,c;
+int n=371;
+c=n;
+while(n>0)
+{
+b=n*10;
+n=n/10;
+a=a+(b*b*b)
+}
+if(c==a)
+{
+System.out.println("YES");
+}
+else
+{
+System.out.println("NO");
+}
+}
+}
